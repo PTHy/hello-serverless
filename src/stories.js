17a1e585679f48ab1bf4de69724b2e98a4700786
PTHy <30455428+PTHy@users.noreply.github.com>
2018-04-19 18:00:21 +0900

    add mongoose
    api, plane project

--- src/stories.js
+++ src/stories.js
@@ -1,3 +1,9 @@
+const mongoose = require('mongoose')
+
+const connect () => {
+  return mongoose.connect('mongodb://localhost/serverless');
+};
+
 const createResponse = (status, body) => ({
   statusCode : status,
   body: JSON.stringify(body)
@@ -5,25 +11,65 @@ const createResponse = (status, body) => ({
 
 //스토리 만들기
 exports.createStory = (event,ctx,cd) => {
-  cb(null, createResponse(200, {message: 'create'}))
-}
+  ctx.callbackWaitsForEmptyEventLoop = false;
+  connect().then(
+    () => {
+        cb(null, createResponse(200, {message: 'create'}));
+    }
+  ).catch
+  (e) => {
+      cb(null, createResponse(500, {message: e}));
+  }
+};
 
 //여러개의 스토리 리스팅
 exports.readStories = (event,ctx,cd) => {
-  cb(null, createResponse(200, {message: 'list'}))
-}
+  ctx.callbackWaitsForEmptyEventLoop = false;
+  connect.then(
+    () => {
+        cb(null, createResponse(200, {message: 'list'}));
+    }
+  ).catch
+  (e) => {
+    cb{null, createResponse(500, {message: e});
+  }
+};
 
 //특정 스토리 읽기
 exports.readStory = (event,ctx,cd) => {
-  cb(null, createResponse(200, {message: 'read'}))
-}
+  ctx.callbackWaitsForEmptyEventLoop = false;
+  connect.then(
+    () => {
+        cb(null, createResponse(200, {message: 'read'}));
+    }
+  ).catch
+    (e) => {
+        cb(null, createResponse(500, {message: e}));
+    }
+};
 
 //스토리 수정
 exports.updateStory = (event,ctx,cd) => {
-  cb(null, createResponse(200, {message: 'update'}))
-}
+  ctx.callbackWaitsForEmptyEventLoop = false;
+  connect.then(
+    () => {
+        cb(null, createResponse(200, {message: 'update'}));
+    }
+  ).catch
+    (e) => {
+      cb(null, createResponse(500, {message: e}));
+    }
+};
 
 //스토리 삭제
 exports.deleteStory = (event,ctx,cd) => {
-  cb(null, createResponse(200, {message: 'delete'}))
-}
+  ctx.callbackWaitsForEmptyEventLoop = false;
+  connect.then(
+    () => {
+        cb(null, createResponse(200, {message : 'delete'}));
+    }
+  ).catch
+    (e) => {
+      cb{null, createResponse(200, {message: e})};
+    }
+};
